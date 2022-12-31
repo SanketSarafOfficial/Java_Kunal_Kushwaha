3a656780d5e7e73ddd4c350800fe76067e51770c
Sanket Saraf <sanket.saraf.official@gmail.com>
2022-12-31 13:14:38 +0530

    Pushed the Main.java test file New Branch Created.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Created on Date 31st December 2022");
     }
 }
